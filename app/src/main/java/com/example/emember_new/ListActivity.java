cab5e8bfd29f71a2563243ea79374c80a6f8d92a
neria <brybrns32@gmail.com>
2022-06-12 20:42:25 +0300

    list added

--- app/src/main/java/com/example/emember_new/ListActivity.java
+++ app/src/main/java/com/example/emember_new/ListActivity.java
@@ -5,14 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class ListActivity extends AppCompatActivity {
+  LinearLayout showAll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
         Bundle bundle= getIntent().getExtras();
+        showAll=findViewById(R.id.SHOW_ALL);
+
 
     }
     public void back(View view)
